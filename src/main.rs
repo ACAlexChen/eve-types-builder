6c7eaeef986dc95110954e5b78f6300a758c14ab
Alex Chen <M.AC.AlexChen@outlook.com>
2024-12-13 19:07:22 +0800

    优化解压函数与压缩函数逻辑，不会产生临时文件

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,21 @@ use std::io;
 use std::path::Path;
 use zip::read::ZipArchive;
 use serde::{Deserialize, Serialize};
-use serde_json;
 use std::collections::HashMap;
 use flate2::write::GzEncoder;
 use flate2::Compression;
+use std::error::Error;
+use std::io::{Read, Write};
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 struct JsonItem {
   id: i32,
   name: Name,
+  #[allow(non_snake_case)]
   groupID: i32
 }
 
-#[derive(Deserialize, Debug, Serialize)]
+#[derive(Deserialize, Debug, Serialize, Clone)]
 struct Name {
   de: Option<String>,
   en: Option<String>,
@@ -31,7 +33,9 @@ struct Name {
 #[derive(Deserialize, Debug)]
 struct Item {
   name: Name,
+  #[allow(non_snake_case)]
   groupID: i32,
+  #[allow(non_snake_case)]
   marketGroupID: Option<i32>
 }
 
@@ -43,80 +47,50 @@ struct Yaml {
 
 fn main() -> () {
   let base_dir = env::current_dir().unwrap();
-  let temp_dir = base_dir.join("temp");
-  if !temp_dir.is_dir() {
-    fs::create_dir_all(temp_dir.to_str().unwrap()).unwrap();
-    let zip_file_path = base_dir.join("sde.zip");
-    unzip(&zip_file_path, &temp_dir).unwrap();
-  }
-  let types_file_path = temp_dir.join("fsd").join("types.yaml");
-  let content = parse_yaml(&types_file_path);
-  println!("{:?}", content);
-  let json = yaml_fmt(&content);
-  let json_str = serde_json::to_string(&json);
-  let json_file_path = base_dir.join("types.json");
-  fs::write(&json_file_path, json_str.unwrap()).unwrap();
-  zip(&json_file_path, &base_dir).unwrap();
-  fs::remove_file(&json_file_path).unwrap();
-  std::thread::sleep(std::time::Duration::from_secs(5));
-  fs::remove_dir_all(temp_dir).unwrap();
+  let zip_file_path = base_dir.join("sde.zip");
+  let out_file_path = base_dir.join("types.json.gz");
+  let yaml_content = get_zip_content(zip_file_path.as_path()).unwrap();
+  let json_content = yaml_fmt(&yaml_content).unwrap();
+  let json_string = serde_json::to_string(&json_content).unwrap();
+  zip(&json_string, out_file_path.as_path()).unwrap();
 }
 
 
-fn unzip(file: &Path, out_dir: &Path) -> io::Result<()> {
+fn get_zip_content(file: &Path) -> Result<Yaml, Box<dyn Error>> {
   let mut archive = ZipArchive::new(fs::File::open(file)?)?;
-  for i in 0..archive.len() {
-    let mut file = archive.by_index(i)?;
-    let file_path = out_dir.join(file.mangled_name());
-    if file.is_dir() {
-      fs::create_dir_all(file_path)?;
-    } else {
-      if let Some(parent) = file_path.parent() {
-        fs::create_dir_all(parent)?;
-      }
-      let mut outfile = fs::File::create(&file_path)?;
-      io::copy(&mut file, &mut outfile)?;
-    }
-  }
-  Ok(())
+  let mut file = archive.by_name("fsd/types.yaml")?;
+  let mut buffer = String::new();
+  if file.is_file() {
+    file.read_to_string(&mut buffer)?;
+  } else {
+    panic!("fsd/types.yaml not found");
+  };
+  parse_yaml(&buffer)
 }
 
-fn parse_yaml(file: &Path) -> Yaml {
-  let file = fs::File::open(file).unwrap();
-  let reader = io::BufReader::new(file);
-  serde_yaml::from_reader(reader).unwrap()
+fn parse_yaml(content: &str) -> Result<Yaml, Box<dyn Error>> {
+  Ok(serde_yaml::from_str(content)?)
 }
 
-fn yaml_fmt(yaml: &Yaml) -> Vec<JsonItem> {
+fn yaml_fmt(yaml: &Yaml) -> Result<Vec<JsonItem>, Box<dyn Error>> {
   let mut items: Vec<JsonItem> = Vec::new();
   for (key, item) in &yaml.values {
-    println!("{:?}: {:?}", key, item);
     if item.marketGroupID.is_some() {
       let json_item = JsonItem {
-        id: key.parse().unwrap(),
-        name: Name {
-          de: item.name.de.clone(),
-          en: item.name.en.clone(),
-          es: item.name.es.clone(),
-          fr: item.name.fr.clone(),
-          ja: item.name.ja.clone(),
-          ko: item.name.ko.clone(),
-          ru: item.name.ru.clone(),
-          zh: item.name.zh.clone()
-        },
+        id: key.parse()?,
+        name: item.name.clone(),
         groupID: item.groupID
       };
       items.push(json_item);
     }
   }
-  items
+  Ok(items)
 }
-fn zip(file: &Path, out_dir: &Path) -> io::Result<()> {
-  let file_data = fs::File::open(file)?;
-  let mut reader = io::BufReader::new(file_data);
-  let output_file = fs::File::create(out_dir.join(format!("{}.gz", file.file_name().unwrap().to_str().unwrap())))?;
+
+fn zip(content: &str, out_file_path: &Path) -> io::Result<()> {
+  let output_file = fs::File::create(out_file_path)?;
   let mut encoder = GzEncoder::new(output_file, Compression::default());
-  io::copy(&mut reader, &mut encoder)?;
+  encoder.write_all(content.as_bytes())?;
   let _ = encoder.finish()?;
   Ok(())
 }
